951df6b68cc4d2a54c6069c65eb3b6ccb392e3dd
Concourse <nobody@concourse.ci>
2017-02-24 06:20:32 +0000

    Update report

---
+++ org/ababup1192/HelloTest.java
@@ -0,0 +1,12 @@
+package org.ababup1192;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloTest {
+    @Test
+    public void passedTest() {
+        assertEquals(true, true);
+    }
+}
